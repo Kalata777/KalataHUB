007e10e0f2c3677d1cf57e1b0081b0c4119613c2
Kalata777 <72138487+Kalata777@users.noreply.github.com>
2020-10-02 17:22:32 +0300

    Create zad2.java

---
+++ zad2.java
@@ -0,0 +1,12 @@
+package kkk;
+import javax.swing.JFrame; 
+public class zad2 {
+	public static int kalata(int a, int b){
+		return a*b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
