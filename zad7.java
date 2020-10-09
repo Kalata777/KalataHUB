9a593dd1dcf7e436d9ea86c2c6c86c247b269ea7
Kalata777 <72138487+Kalata777@users.noreply.github.com>
2020-10-09 12:44:56 +0300

    Create zad7.java

---
+++ zad7.java
@@ -0,0 +1,20 @@
+package greativity;
+
+public class zad7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=7;i>0;i--) {
+			for(int m=7-i;m>0;m--) {
+				System.out.print(" ");
+			}
+			for(int j=7-i;j<7;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
